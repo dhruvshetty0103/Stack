e8171d52f09f77af1e329789ec56a33156f6b620
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-09-08 19:50:41 +0530

    Added:Creating Queue and adding node at last queue using linked list

--- Queue.java
+++ Queue.java
@@ -1,7 +1,13 @@
 package com.linkedlist.stack;
-public class Queue 
+public class Queue<K> 
 {
-	public static class Node 
+	static Node head; // head of list
+	 
+    /* Linked list Node
+     * Constructor to create a new node
+     * Next is by default initialized as null
+    */
+    public static class Node 
     {
         int data;
         Node next;
@@ -10,5 +16,53 @@ public class Queue
         	data = d;
         }
     }
-	
+    // Method to insert a new node
+    public static Queue insert(Queue list, int data)
+    {
+        // Create a new node with given data
+    	Node newNode = new Node(data);
+        newNode.next = null;
+        // If the Linked List is empty,then make the new node as head
+        if (list.head == null) 
+        	list.head = newNode;
+        else 
+        {
+        	// Else traverse till the last node and insert the new_node there and Insert the newNode at last node
+            Node last = list.head;
+            while (last.next != null) 
+            	last = last.next;
+            last.next = newNode;
+        }
+        // Return the list by head
+        return list;
+    }
+   
+    // Method to print the LinkedList.
+    public static void printList(Queue list)
+    {
+        Node currNode = list.head;
+        System.out.print("LinkedList: ");
+    
+        // Traverse through the LinkedList
+        while (currNode != null) 
+        {
+            // Print the data at current node and go to next node
+            System.out.print(currNode.data + " ");
+            currNode = currNode.next;
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        /* Start with the empty list. */
+        Queue list = new Queue();
+   
+        // Insert the values
+        list = insert(list,56);
+        list = insert(list,30);
+        list = insert(list,70);
+   
+        // Print the LinkedList
+        printList(list);
+    }
 }
