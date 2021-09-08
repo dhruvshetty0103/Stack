d0429eb718047ff3dff3a267ca0b9317f9058632
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-09-08 18:45:02 +0530

    Added:Creating Node with the reference to linked list

---
+++ Queue.java
@@ -0,0 +1,14 @@
+package com.linkedlist.stack;
+public class Queue 
+{
+	public static class Node 
+    {
+        int data;
+        Node next;
+        Node(int d) 
+        { 
+        	data = d;
+        }
+    }
+	
+}
